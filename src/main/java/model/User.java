748eabd3bf0824c25a4a4d520c966ed47fe3f6e5
tomishima <pumoco2904@icloud.com>
2023-03-10 22:00:45 +0900

    完成: `set`はしない. `pass`は引数のものを入れる.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -10,12 +10,10 @@ public class User implements Serializable{
 	
 	public User(String name, String pass) {
 		this.name = name;
-		this.pass = "1234";
+		this.pass = pass;
 	}
 	
 	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
 	public String getPass() { return pass; }
-	public void setPass(String pass) { this.pass = pass; }
 	
 }
