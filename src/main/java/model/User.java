f6d1c458fa66b6fe4b8f7ae45f1e957701bdb210
tomishima <pumoco2904@icloud.com>
2023-03-10 21:59:30 +0900

    `pass`はString型に

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,19 +3,19 @@ import java.io.Serializable;
 
 public class User implements Serializable{
 	private String name;
-	private int pass;
+	private String pass;
 	
 
 	public User( ) {}
 	
-	public User(String name, int pass) {
+	public User(String name, String pass) {
 		this.name = name;
-		this.pass = 1234;
+		this.pass = "1234";
 	}
 	
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
-	public int getPass() { return pass; }
-	public void setPass(int pass) { this.pass = pass; }
+	public String getPass() { return pass; }
+	public void setPass(String pass) { this.pass = pass; }
 	
 }
